4e2947068902e55fd1b59ab52649225920f292fd
KrytoiRater <aroslavseleznev933@gmail.com>
2026-07-22 14:10:47 +0400

    Update fps flick script.lua

--- fps flick script.lua
+++ fps flick script.lua
@@ -58,7 +58,7 @@ corner.CornerRadius = UDim.new(1, 0)
 corner.Parent = circle
 
 RunService.RenderStepped:Connect(function()
-    local c = camera.ViewportSize / 1
+    local c = camera.ViewportSize / 3
     circle.Position = UDim2.new(0, c.X - 200, 0, c.Y - 200)
 end)
 
